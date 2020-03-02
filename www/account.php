cf8c141018d9d799462d128269279ad1f1d5f3a4
maichongju <31859452+maichongju@users.noreply.github.com>
2020-03-02 13:33:35 -0500

    updated account.php

--- www/account.php
+++ www/account.php
@@ -20,6 +20,19 @@ session_start();
     <?php require_once "sidebar.php" ?>
 
     <div class="main-content">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" id="form-change-password">
+            <h2 class="title-2">Change Password</h2>
+            <div class="break-line"></div>
+            <label>
+                Old Password
+                <input type="password" name="old-password">
+            </label>
+
+
+        </form>
+
+
+
     </div>
 
 
