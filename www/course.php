ad6489c61b72cfdbad42fe41e4bf5c9d7d7dcb06
zhw <yuan4420@gmail.com>
2020-03-03 18:29:01 -0500

    course struct#2

--- www/course.php
+++ www/course.php
@@ -8,14 +8,45 @@ session_start();
     <link href="css/project.css" rel="stylesheet">
 </head>
 <body>
-    <?php require_once "topbar.php" ?>
-    <div>
-        <?php require_once "sidebar.php" ?>
-        <div class="main-content">
+<?php require_once "topbar.php" ?>
+<div>
+    <?php require_once "sidebar.php" ?>
+    <div class="main-content">
+        <div class="items">
+            <header class="file-title">assignment1</header>
+            <img class="file-icon" src="images/icon-word.png">
+            <div class="file-but-containor">
+                <button class="file-but">Preview</button>
+                <button class="file-but">Comment</button>
+                <button class="file-but">Download</button>
+            </div>
 
         </div>
+        <div class="items">
+            <header class="file-title">assignment2</header>
+            <img class="file-icon" src="images/icon-word.png">
+            <div class="file-but-containor">
+                <button class="file-but">Preview</button>
+                <button class="file-but">Comment</button>
+                <button class="file-but">Download</button>
+            </div>
+
+        </div>
+        <div class="items">
+            <header class="file-title">lecture1</header>
+            <img class="file-icon" src="images/icon-word.png">
+            <div class="file-but-containor">
+                <button class="file-but">Preview</button>
+                <button class="file-but">Comment</button>
+                <button class="file-but">Download</button>
+            </div>
+
+        </div>
+
     </div>
 
+</div>
+</div>
 
 
 </body>
