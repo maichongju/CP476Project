1f97b29cadf14c17bc7ed20c94021a67df5a4615
zhw <yuan4420@gmail.com>
2020-03-03 18:02:10 -0500

    course struct

--- www/course.php
+++ www/course.php
@@ -1,1 +1,21 @@
 <?php
+session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <link href="css/project.css" rel="stylesheet">
+</head>
+<body>
+    <?php require_once "topbar.php" ?>
+    <div>
+        <?php require_once "sidebar.php" ?>
+        <div class="main-content">
+
+        </div>
+    </div>
+
+
+
+</body>
