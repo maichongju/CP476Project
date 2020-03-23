9052200d4083914229f5404159c19f4464529cb0
github <maichongju>
2020-03-23 18:05:30 -0400

    Added Bootstrap

---
+++ www/include/header.php
@@ -0,0 +1,10 @@
+<!-- JQuery -->
+<script src="js/jquery-3.4.1-min.js"></script>
+<!-- BootsStrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<script src="js/bootstrap.js"></script>
+<!-- Iconic -->
+<link href="open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+<!-- Global CSS -->
+<link href="css/project.css" rel="stylesheet">
+<!-- Other -->
--- www/login.php
+++ www/login.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $user = userUtil::Login($username, $password);
         if ($user === null) {
             $error_msg = ErrorMsg::DATABASE_USERNAME_PASSWORD_ERROR;
-        }else{
+        } else {
             // login success
             session_start();
             $_SESSION["loggedin"] = true;
@@ -41,30 +41,69 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 <head>
     <title>Log In</title>
-    <link href="css/project.css" rel="stylesheet">
+    <?php include "include/header.php" ?>
     <link href="css/form.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body class="blackground">
-<div id="div-login-form">
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" id="form-login">
-        <div>
-            <p class="form-title">Log in</p>
-            <p>Username</p>
-            <input type="text" name="username" placeholder="Username" required>
-            <p>Password</p>
-            <input type="password" name="password" placeholder="Password" , required>
-            <?php if ($error_msg !== null) {
-                echo "<p class=\"form-error-msg\">" . $error_msg . "</p>";
-            } ?>
-            <input type="submit" class="form-submit">
-            <p id="sign-up-hint">Don't have an account? Sign up <a href="signup.php">here</a></p>
+
+<!--        <form class="form mx-auto" action="login.php" method="post">-->
+<!--            <div>-->
+<!--                <p class="form-title">Log in</p>-->
+<!--                <p>Username</p>-->
+<!--                <input type="text" name="username" placeholder="Username" required>-->
+<!--                <p>Password</p>-->
+<!--                <input type="password" name="password" placeholder="Password" , required>-->
+<!--                --><?php //if ($error_msg !== null) {
+//                    echo "<p class=\"form-error-msg\">" . $error_msg . "</p>";
+//                } ?>
+<!--                <input type="submit" class="form-submit">-->
+<!--                <p id="sign-up-hint">Don't have an account? Sign up <a href="signup.php">here</a></p>-->
+<!--            </div>-->
+<!---->
+<!--        </form>-->
+<body>
+<section id="cover" class="min-vh-100">
+    <div id="cover-caption">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
+
+                    <h1 class="display-4 py-2 text-truncate text-white">Log in</h1>
+                    <div class="px-2">
+                        <form action="login.php" method="post" class="justify-content-center">
+                            <div class="form-group">
+                                <label class="sr-only" for="usernameinput">Username</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                    <span class="input-group-text"><span class="oi oi-person" title="person"
+                                                                         aria-hidden="true"></span></span>
+                                    </div>
+                                    <input id="usernameinput" name="username" type="text" class="form-control"
+                                           placeholder="Username">
+                                </div>
+
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only" for="passwordinput">Password</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                    <span class="input-group-text"><span class="oi oi-lock-locked" title="lock-locked"
+                                                                         aria-hidden="true"></span></span>
+                                    </div>
+                                    <input id="passwordinput" name="password" type="password" class="form-control"
+                                           placeholder="password">
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-lg">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-
-    </form>
-</div>
-
-
+    </div>
+</section>
 </body>
+
 
 </html>
