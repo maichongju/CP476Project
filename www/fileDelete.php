2648ac5520359cac9b5cfb90c5068508b6728a79
github <maichongju>
2020-04-19 01:28:20 -0400

    added remove function on course

--- www/fileDelete.php
+++ www/fileDelete.php
@@ -1,17 +1,17 @@
 <?php
 
-if (!empty($_POST)){
+if (!empty($_POST)) {
     require_once "util/fileUtil.php";
     session_start();
-    $result = array("result"=>false);
-    if (isset($_SESSION["loggedin"]) && isset($_SESSION["courseid"]) && isset($_SESSION["userid"]) && isset($_POST["fileid"])){
-        $r = fileUtil::delete($_SESSION["userid"],$_SESSION["courseid"],$_POST["fileid"]);
-        if ($r["result"]){
+    $result = array("result" => false);
+    if (isset($_SESSION["loggedin"]) && isset($_SESSION["courseid"]) && isset($_SESSION["userid"]) && isset($_POST["fileid"])) {
+        $r = fileUtil::delete($_SESSION["userid"], $_SESSION["courseid"], $_POST["fileid"]);
+        if ($r["result"]) {
             $result["result"] = true;
             $path = $r["path"];
             $courseid = $_SESSION["courseid"];
             $fullpath = "file/$path";
-            if (file_exists($fullpath)){
+            if (file_exists($fullpath)) {
                 unlink($fullpath);
             }
         }
